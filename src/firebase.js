6f1da7c46be02ad85766cfc4a74b7bf7546cfef6
HEMANTVS05 <vshemant05@gmail.com>
2026-09-25 11:18:16 +0530

    DONESSSS

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFIxeW8SyNp3we2ogNbhDQ_DVz3OFHJ2g",
-  authDomain: "startup-peravai.firebaseapp.com",
-  projectId: "startup-peravai",
-  storageBucket: "startup-peravai.firebasestorage.app",
-  messagingSenderId: "188162882355",
-  appId: "1:188162882355:web:df573dfb12d07605645c46",
-  measurementId: "G-RT5TMZWDS7",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
